a1b3ac1d902cd9a4f939fb16bd33581b63ed96dc
BrandonGreet <greet.brandon@yahoo.com>
2020-02-09 01:25:14 -0500

    OSMREquest added and implemented for OSMApiManager

--- src/main/java/ApiUtil/OsmApiManager.java
+++ src/main/java/ApiUtil/OsmApiManager.java
@@ -1,9 +1,17 @@
 package ApiUtil;
 
+import org.eclipse.jetty.util.IO;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class OsmApiManager extends ApiManager {
 
     public static void main( String[] args ) {
 
     }
 
+    public InputStream makeRequest(OSMRequest r) throws IOException {
+        return getInputStream(r);
+    }
 }
